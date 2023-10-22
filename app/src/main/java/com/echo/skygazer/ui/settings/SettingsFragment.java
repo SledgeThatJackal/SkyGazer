62e78b8041bee35851c69e65a4fa6b499abcb1ab
Alan DAmico <adamico1@uncc.edu>
2023-10-22 14:40:20 -0400

    Create Preference Page / Persistent settings

--- app/src/main/java/com/echo/skygazer/ui/settings/SettingsFragment.java
+++ app/src/main/java/com/echo/skygazer/ui/settings/SettingsFragment.java
@@ -1,43 +1,23 @@
 package com.echo.skygazer.ui.settings;
 
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
 import androidx.preference.PreferenceFragmentCompat;
 
 import com.echo.skygazer.R;
-import com.echo.skygazer.databinding.FragmentSettingsBinding;
 
 public class SettingsFragment extends PreferenceFragmentCompat {
 
-    private FragmentSettingsBinding binding;
-
     @Override
     public void onCreatePreferences(@Nullable Bundle savedInstanceState, @Nullable String rootKey) {
+        // Grabs the settings and sets the preferences to those values
+        // Preferences are saved under skygazer_preferences.xml
         setPreferencesFromResource(R.xml.preferences, rootKey);
-    }
-
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        SettingsViewModel notificationsViewModel =
-                new ViewModelProvider(this).get(SettingsViewModel.class);
-
-        binding = FragmentSettingsBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
-
-        return root;
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
-        binding = null;
     }
 }
