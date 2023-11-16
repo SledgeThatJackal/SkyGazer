b6375028ca2d273301df43daa7301081d5bbc172
Alan DAmico <adamico1@uncc.edu>
2023-11-16 09:10:38 -0500

    Fixed Methods and added comments

--- app/src/main/java/com/echo/skygazer/io/Constellations.java
+++ app/src/main/java/com/echo/skygazer/io/Constellations.java
@@ -2,99 +2,87 @@ package com.echo.skygazer.io;
 
 import static com.echo.skygazer.Main.getMainActivity;
 
-import android.content.Intent;
-import android.content.res.AssetManager;
-import android.util.JsonReader;
 import android.util.Log;
-
-import com.echo.skygazer.MainActivity;
-import com.google.gson.Gson;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Iterator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 public class Constellations {
-    private List<SpecificConstellation> constellations = new ArrayList<>();
+    private HashMap<Integer, SpecificConstellation> constellations = new HashMap<>();
 
     public Constellations() {
         try {
+            // Grab JSON file
             InputStream inputStream = getMainActivity().getAssets().open("constellations.json");
             int size = inputStream.available();
+
+            // Read the JSON file in the InputStream, put it into a byte array, and save it as a string
             byte[] buffer = new byte[size];
             inputStream.read(buffer);
             inputStream.close();
             String json = new String(buffer);
 
+            // Turn the String into a JSON object
             JSONObject jsonObject = new JSONObject(json);
+
+            // Grab the Array within the JSON object
             JSONArray constellationArray = jsonObject.getJSONArray("constellations");
+
             for(int i = 0; i < constellationArray.length(); i++){
                 JSONObject constellationInfo = constellationArray.getJSONObject(i);
-                int constellationID = constellationInfo.getInt("constellationID");
+
+                // Gather information
+                int constellationId = constellationInfo.getInt("constellationID");
                 String constellationName = constellationInfo.getString("constellationName");
                 String link = constellationInfo.getString("link");
 
+                // Break up link String and convert all of the strings into integers
+                // This is to make it easier to highlight the constellations in the AR portion
                 String[] strStars = link.split(" ");
                 int[] stars = new int[strStars.length];
                 for(int k = 0; k < strStars.length; k++) {
                     stars[k] = Integer.parseInt(strStars[k]);
                 }
 
-                constellations.add(new SpecificConstellation(constellationID, constellationName, stars));
+                // Put each Constellation object into a HashMap
+                constellations.put(constellationId, new SpecificConstellation(constellationId, constellationName, stars));
             }
         } catch (IOException e) {
-            Log.d("Constellation Json File", "Something went wrong.");
+            Log.d("JSON- IOException", Objects.requireNonNull(e.getMessage()));
         } catch (JSONException e) {
-            Log.d("Constellation Json File", e.getMessage());
+            Log.d("JSON- JSONException", Objects.requireNonNull(e.getMessage()));
         }
-
-        Log.d("Constellation List", constellations.toString());
     }
 
+    public HashMap<Integer, SpecificConstellation> getConstellations() {
+        return constellations;
+    }
 
+    // NOTE: The return type on this is a List because there are some Constellations that have duplicate stars
+    public List<SpecificConstellation> getConstellation(int starId){
+        // An ArrayList that will contain all of the constellation(s) that contain a specific star
+        List<SpecificConstellation> starConstellation = new ArrayList<>();
 
-    public String getConstellationName(int id){
-        for(SpecificConstellation constellation: constellations){
-            if(constellation.getConstellationID() == id){
-                return constellation.getConstellationName();
+        // Loops through constellation dictionary
+        for(Map.Entry<Integer, SpecificConstellation> currEntry: constellations.entrySet()){
+
+            // Loop through link array and compare the provided argument to the stars in the constellation
+            int[] link = currEntry.getValue().getLink();
+            for(int i = 0; i < link.length; i++){
+                if(link[i] == starId){
+                    starConstellation.add(currEntry.getValue());
+                }
             }
         }
 
-        return "";
+        return starConstellation;
     }
-
-    public int getConstellationId(String name){
-        for(SpecificConstellation constellation: constellations){
-            if(constellation.getConstellationName().equals(name)){
-                return constellation.getConstellationID();
-            }
-        }
-
-        return -1;
-    }
-
-    public int[] getConstellationStars(int id){
-        for(SpecificConstellation constellation: constellations){
-            if(constellation.getConstellationID() == id){
-                return constellation.getLink();
-            }
-        }
-
-        return null;
-    }
-
-
 }
