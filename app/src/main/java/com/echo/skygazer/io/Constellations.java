55dde3040163c97ed97b83ff5c05e9c763397091
Alan DAmico <adamico1@uncc.edu>
2023-11-16 09:35:51 -0500

    Removing unnecessary comments, changing variable names, and fixing for loop.

--- app/src/main/java/com/echo/skygazer/io/Constellations.java
+++ app/src/main/java/com/echo/skygazer/io/Constellations.java
@@ -20,39 +20,32 @@ public class Constellations {
 
     public Constellations() {
         try {
-            // Grab JSON file
             InputStream inputStream = getMainActivity().getAssets().open("constellations.json");
             int size = inputStream.available();
 
-            // Read the JSON file in the InputStream, put it into a byte array, and save it as a string
             byte[] buffer = new byte[size];
             inputStream.read(buffer);
             inputStream.close();
             String json = new String(buffer);
 
-            // Turn the String into a JSON object
             JSONObject jsonObject = new JSONObject(json);
-
-            // Grab the Array within the JSON object
             JSONArray constellationArray = jsonObject.getJSONArray("constellations");
 
             for(int i = 0; i < constellationArray.length(); i++){
                 JSONObject constellationInfo = constellationArray.getJSONObject(i);
 
-                // Gather information
                 int constellationId = constellationInfo.getInt("constellationID");
                 String constellationName = constellationInfo.getString("constellationName");
-                String link = constellationInfo.getString("link");
+                String links = constellationInfo.getString("links");
 
                 // Break up link String and convert all of the strings into integers
                 // This is to make it easier to highlight the constellations in the AR portion
-                String[] strStars = link.split(" ");
+                String[] strStars = links.split(" ");
                 int[] stars = new int[strStars.length];
                 for(int k = 0; k < strStars.length; k++) {
                     stars[k] = Integer.parseInt(strStars[k]);
                 }
 
-                // Put each Constellation object into a HashMap
                 constellations.put(constellationId, new SpecificConstellation(constellationId, constellationName, stars));
             }
         } catch (IOException e) {
@@ -67,18 +60,15 @@ public class Constellations {
     }
 
     // NOTE: The return type on this is a List because there are some Constellations that have duplicate stars
-    public List<SpecificConstellation> getConstellation(int starId){
-        // An ArrayList that will contain all of the constellation(s) that contain a specific star
+    public List<SpecificConstellation> getConstellationFromStarId(int starId){
         List<SpecificConstellation> starConstellation = new ArrayList<>();
 
-        // Loops through constellation dictionary
         for(Map.Entry<Integer, SpecificConstellation> currEntry: constellations.entrySet()){
-
-            // Loop through link array and compare the provided argument to the stars in the constellation
-            int[] link = currEntry.getValue().getLink();
-            for(int i = 0; i < link.length; i++){
-                if(link[i] == starId){
+            int[] links = currEntry.getValue().getLinks();
+            for(int i = 0; i < links.length; i++){
+                if(links[i] == starId){
                     starConstellation.add(currEntry.getValue());
+                    break;
                 }
             }
         }
